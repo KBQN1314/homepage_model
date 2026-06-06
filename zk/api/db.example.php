8087a0c1ed6d1ff765ac17827608ce91fe63c78d
KBQN1314 <kbqn1314@gmail.com>
2026-06-07 02:25:53 +0800

    Add database config template

---
+++ zk/api/db.example.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 复制本文件为 db.php，并在服务器上填写真实数据库信息。
+ * 注意：db.php 不要提交到公开仓库。
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'db_zhongkemingxin',
+    'username' => 'db_zhongkemingxin',
+    'password' => '请在服务器上填写真实密码',
+    'charset' => 'utf8mb4',
+];
